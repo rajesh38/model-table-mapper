c942561d18bd210c39b2567f10e8e3b6e4ef07e2
Rajesh Paul <rajesh.paul@letsventure.com>
2017-04-23 16:37:31 +0530

    support added for ActiveRecord::Base

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ __steps:__
 
 ## Contributing
 
-Bug reports and pull requests are welcome on GitHub at [https://github.com/rajesh38/model_table_mapper](https://github.com/rajesh38/model_table_mapper). This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
+Bug reports and pull requests are welcome on GitHub at [https://github.com/rajesh38/model-table-mapper](https://github.com/rajesh38/model-table-mapper). This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
 
 
 ## License
--- lib/model_table_mapper.rb
+++ lib/model_table_mapper.rb
@@ -1,11 +1,16 @@
 require "model_table_mapper/version"
+require "find"
 
 module ModelTableMapper
   def self.list(*args)
-    model_files = %x(ls app/models/*.rb).split
+    model_files = []
+    Find.find('app/models') do |path|
+      model_files << path if path =~ /.*\.rb/
+    end
     models = model_files.map do |file_name|
-      class_statement = File.open("#{file_name}").read.match(/^class +\w+ *< *ApplicationRecord\s*/).try(:[], 0)
-      class_statement.match(/class +(\w+)/).try(:[], 1) if class_statement.present?
+      file_content = File.open("#{file_name}").read
+      class_statement = file_content.match(/^class +[\w:]+ *< *(ApplicationRecord|ActiveRecord::Base)\s*/).try(:[], 0)
+      class_statement.match(/class +([\w:]+)/).try(:[], 1) if class_statement.present?
     end.compact
     model_table_hash = models.each_with_object({}) do |model, result|
       result[model] = model.constantize.table_name
